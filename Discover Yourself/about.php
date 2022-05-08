39f3861a66d48d4970e7581e1cf0b9aba0fd2f91
rabinaitgithub <104816005+rabinaitgithub@users.noreply.github.com>
2022-05-08 10:47:14 +0545

    Add files via upload

---
+++ Discover Yourself/about.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+ 
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+ 
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+
+
+<section class="header">
+
+   <a href="home.php" class="logo">Travel.</a>
+
+   <nav class="navbar">
+      <a href="home.php">Home</a>
+      <a href="about.php">About</a>
+      <a href="package.php">Package</a>
+      <a href="book.php">Book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+
+
+<div class="heading" style="background:url(images/header-bg-1.png) no-repeat">
+   <h1>About Us</h1>
+</div>
+
+
+
+<section class="about">
+
+   <div class="image">
+      <img src="images/about-img.jpg" alt="">
+   </div>
+
+   <div class="content">
+      <h3>Why Us?</h3>
+      <p> We offer an independent, friendly, reliable, comprehensive and safe trekking service at very aggressive rates. We have been providing quality travel services from long time. We provide mountaineering, peak climbing, rafting/kayaking, sightseeing tour, jungle safari, bird watching, mountain flight, rock climbing, hot air ballooning, cable car, mountain biking, paragliding, trekking & trips and many more. </p>
+      <p>We conceit flexibility in arranging tours to meet your individual needs. We provide a unique experience to our guests.</p>
+      <div class="icons-container">
+         <div class="icons">
+            <i class="fas fa-map"></i>
+            <span>Top destinations</span>
+         </div>
+         <div class="icons">
+            <i class="fas fa-hand-holding-usd"></i>
+            <span>Affordable price</span>
+         </div>
+         <div class="icons">
+            <i class="fas fa-headset"></i>
+            <span>24/7 Guide Service</span>
+         </div>
+      </div>
+   </div>
+
+</section>
+
+>
+
+<section class="reviews">
+
+   <h1 class="heading-title"> Clients Reviews </h1>
+
+   <div class="swiper reviews-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>OT&T provides the best hospitality in comparisiopn to the others with the outstanding discount rate.All of the staff were professional and very nice. On Guide were very organized and always there for us if we had any questions. We always felt safe, well taken care of with a very friendly attitude from crew while they showed us a great time, both fun and educational. </p>
+            <h3>Krishna Magar</h3>
+            <span>Traveler</span>
+            <img src="images/intern1.jpg" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>We had the most remarkable family tour with OT & T Travels. Not only everything was perfect, but we felt, in every instance, that our guides and our gulet crew truly cared about us, both the kids and adults, and did their best to make this trip memorable.We definitely will be back!</p>
+            <h3>Pabitra Soti</h3>
+            <span>Traveler</span>
+            <img src="images/intern2.jpg" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>The comfort and beauty of the gulet, the incredibly caring crew, the delicious food, the history we learned from our competent and friendly leader, along with the stunning beauty of the mountains. Swimming, rafting, hiking around ruins and into small remote villages and relaxing on the boat... it was the best trip ever.</p>
+            <h3>Jamuna Gurung</h3>
+            <span>Traveler</span>
+            <img src="images/intern3.jpg" alt="">
+         </div>
+
+         
+
+      </div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> Package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> Book</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +977 6838369381 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +977 9866457654 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> rabinabhandari2018@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> Kathmandu, Nepal </a>
+      </div>
+
+      <div class="box">
+         <h3>Follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>
+         <a href="#"> <i class="fab fa-youtube"></i> Youtube </a>
+         <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+        
+      </div>
+
+   </div>
+
+   <div class="credit"> Created By <span>Rabina Bhandari</span> | All Rights Reserved! </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ Discover Yourself/home.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Home</title>
+
+  
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+  
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+
+
+<section class="header">
+
+   <a href="home.php" class="logo">Travel.</a>
+
+   <nav class="navbar">
+      <a href="home.php">Home</a>
+      <a href="about.php">About</a>
+      <a href="package.php">Package</a>
+      <a href="book.php">Book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+
+
+
+
+<section class="home">
+
+   <div class="swiper home-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide" style="background:url(images/img2.PNG) no-repeat">
+            <div class="content">
+               <span>Explore, Discover, Travel</span>
+               <h3>TRAVEL AROUND THE WORLD</h3>
+               <a href="package.php" class="btn">Discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/img-10.jpg) no-repeat">
+            <div class="content">
+               <span>Explore, Discover, Travel</span>
+               <h3>DISCOVER THE NEW PLACES</h3>
+               <a href="package.php" class="btn">Discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/home-slide-3.jpg) no-repeat">
+            <div class="content">
+               <span>Explore, Discover, Travel</span>
+               <h3>Make Your Tour Worthwhile</h3>
+               <a href="package.php" class="btn">Discover more</a>
+            </div>
+         </div>
+         
+      </div>
+
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+
+   </div>
+
+</section>
+
+
+
+<section class="services">
+
+   <h1 class="heading-title"> Our Services </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/icon-1.png" alt="">
+         <h3>Adventure</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-2.png" alt="">
+         <h3>Tour Guide</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-3.png" alt="">
+         <h3>Trekking</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-4.png" alt="">
+         <h3>Camp Fire</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-5.png" alt="">
+         <h3>Off Road</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-6.png" alt="">
+         <h3>Camping</h3>
+      </div>
+
+   </div>
+
+</section>
+
+
+<section class="home-about">
+
+   <div class="image">
+      <img src="images/about-img.jpg" alt="">
+   </div>
+
+   <div class="content">
+      <h3>About Us</h3>
+      <p>Welcome and Namaste on behalf of OT&T. We offer an independent, friendly, reliable, comprehensive and safe trekking service at very aggressive rates. </p>
+      <a href="about.php" class="btn">read more</a>
+   </div>
+
+</section>
+
+
+
+<section class="home-packages">
+
+   <h1 class="heading-title"> Our Packages </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-1.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Discover Yourself</h3>
+            <p>OT & T Provides the Best Opportunity to Explore the Beautiful World and to Discover Ourself</p>
+            <a href="book.php" class="btn">Book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-2.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Discover Yourself</h3>
+            <p>OT & T Provides the Best Opportunity to Explore the Beautiful World and to Discover Ourself</p>
+            <a href="book.php" class="btn">Book now</a>
+         </div>
+      </div>
+      
+      <div class="box">
+         <div class="image">
+            <img src="images/intern6.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Discover Yourself</h3>
+            <p>OT & T Provides the Best Opportunity to Explore the Beautiful World and to Discover Ourself</p>
+            <a href="book.php" class="btn">Book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/intern2.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Discover Yourself</h3>
+            <p>OT & T Provides the Best Opportunity to Explore the Beautiful World and to Discover Ourself</p>
+            <a href="book.php" class="btn">Book now</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"> <a href="package.php" class="btn">Load more</a> </div>
+
+</section>
+
+
+
+<section class="home-offer">
+   <div class="content">
+      <h3>upto 25% off</h3>
+      <p>OT & T Provides the Best Opportunity to Explore the Beautiful World with the Great Hospitality and the great chance to Discover Yourself </p>
+      <a href="book.php" class="btn">Book now</a>
+   </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Quick Links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> Package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> Book</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra Links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +977 6838369381 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +977 9866457654 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> rabinabhandari2018@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> Kathmandu, Nepal </a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>
+         <a href="#"> <i class="fab fa-youtube"></i> Youtube </a>
+         <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+       
+      </div>
+
+   </div>
+
+   <div class="credit"> Created By <span>Rabina Bhandari</span> | All Rights Reserved! </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
